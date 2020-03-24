8d5e7fc0d6f2f0c3ffcca1b2b2725f955544e0e6
Dionino <51341014+Dionino@users.noreply.github.com>
2020-03-24 21:45:57 +0000

    First Commit

---
+++ movie_api/server.js
@@ -0,0 +1,37 @@
+const http = require('http'),
+fs = require('fs'),
+url = require('url');
+
+http.createServer((request, response) => {
+  var addr = request.url,
+  q = url.parse(addr, true);
+  filePath = '';
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile('documentation.html', function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('Added to log.');
+      }
+    });
+    response.writeHead(200, {'Content-type': 'text/plain'});
+    response.write(data);
+    response.end();
+
+  });
+
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
